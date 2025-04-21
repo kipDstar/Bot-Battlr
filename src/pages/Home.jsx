d40574e579ba9c47a7bedb6c84d5d8715135873a
Jesse Kipsang <kipsangjesse17@gmail.com>
2025-04-21 07:13:23 +0300

    updated Home.jsx to Display YourBotArmy component on the Home page above bot collection

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
-// src/pages/Home.jsx
-import React from 'react';
-import BotCollection from '../components/BotCollection/BotCollection';
-import FilterBar from '../components/FilterBar/FilterBar';
-import SortBar from '../components/SortBar/SortBar';
+import React from "react";
+import "./Home.css";
+import FilterBar from "../components/FilterBar/FilterBar";
+import SortBar from "../components/SortBar/SortBar";
+import BotCollection from "../components/BotCollection/BotCollection";
+import YourBotArmy from "../components/YourBotArmy/YourBotArmy";
 
-const Home = () => {
+function Home() {
   return (
     <div className="home-page">
+      <YourBotArmy />
       <FilterBar />
       <SortBar />
       <BotCollection />
     </div>
   );
-};
+}
 
 export default Home;
+// This is the main page of the application, where users can view their bot army,
+// filter and sort the bot collection, and manage their bots.
+// The Home component imports and uses the YourBotArmy, FilterBar, SortBar, and BotCollection components.
